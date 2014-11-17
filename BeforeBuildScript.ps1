f5e68edcba2d255608385c2063306a6a7a7eaf99
Feodor Fitsner <feodor@fitsner.com>
2014-11-17 10:19:18 -0800

    Temp certificate added

--- BeforeBuildScript.ps1
+++ BeforeBuildScript.ps1
@@ -1,7 +1,1 @@
-$psw = (Get-ItemProperty -path HKCU:\SOFTWARE\Tulpep).PfxPassword
-if(!$psw)
-{
-throw "HKCU\Software\Tulpep\PfxPassword does not exist"
-}
-$psw = ConvertTo-SecureString $psw -AsPlainText -Force
-Import-PfxCertificate -FilePath .\Tulpep-Certificate.pfx -CertStoreLocation cert:\CurrentUser\My -Password $psw
+Import-PfxCertificate -FilePath .\Tulpep-Certificate.pfx -CertStoreLocation cert:\CurrentUser\My
